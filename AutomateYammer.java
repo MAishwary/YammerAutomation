6034d513fc508de09b7fc93b5a3dc5da1f4a485f
MAishwary <107802908+MAishwary@users.noreply.github.com>
2022-07-18 09:55:48 +0530

    Add files via upload

---
+++ AutomateYammer.java
@@ -0,0 +1,67 @@
+package YammerAutomation;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager; 
+
+public class AutomateYammer 
+{
+	@Test
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		WebDriver driver = null;
+		try
+		   {
+			WebDriverManager.chromedriver().setup();
+			 WebDriver driver1 = new ChromeDriver();
+			 driver1.manage().window().maximize();
+				driver1=new ChromeDriver();
+				driver1.get("https://web.yammer.com/main/capgemini.com/");
+				driver1.manage().window().maximize();
+				driver1.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
+				System.out.println("PASSED: User entered into the website!!!");
+		   }
+		   catch(Exception e)
+		   {
+			   System.out.println("EXCEPTION FOUND: Cannot open the browser!!!");
+			   Assert.fail();
+		   }
+		try
+		   {			  
+				driver.findElement(By.xpath("//input[@type='email']")).sendKeys("m.a.aishwarya@capgemini.com");
+				Thread.sleep(3000);
+				driver.findElement(By.xpath("//input[@type='submit']")).click();
+				driver.findElement(By.id("idBtn_Back")).click();
+				Thread.sleep(3000);				
+				System.out.println("PASSED: User logged into Yammer!!!");
+		   }
+		   catch(Exception e)
+		   {
+			   System.out.println("EXCEPTION FOUND: Cannot enter into Yammer!!!");
+			   Assert.fail();
+		   }
+		try
+		   {	
+				driver.findElement(By.xpath("//div[text()='OneTeamOneFamily']")).click();
+				//driver.findElement(By.xpath("//input[@type='button']")).click();
+				System.out.println("PASSED: User entered into the OTOF group!!!");
+				driver.findElement(By.xpath("//div[@class='inner-204']")).sendKeys(" ");
+				System.out.println("PASSED: User entered into the Post content tab!!!");
+		   }
+		   catch(Exception e)
+		   {
+			   System.out.println("EXCEPTION FOUND: Cannot click OTOF group!!!");
+			   Assert.fail();
+		   }
+		
+	}
+
+}
